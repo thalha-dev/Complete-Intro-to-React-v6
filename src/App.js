5899ecff3f11cbaab1e613a40d4cb07543ae847e
thalha-dev <dev.thalha@gamil.com>
2021-11-28 15:35:58 +0530

    JSX and eslint config

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cocokatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      breed: "Wheaten Terrier",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cocokatiel" />
+      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
+    </div>
+  );
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
